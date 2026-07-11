aadaa0b1812ef6207ab654be6c5882eb92a650a6
Amirullah <amir@pnl.ac.id>
2026-07-11 03:46:32 +0000

    chore: perbarui GeneratedPluginRegistrant (soundpool)

--- kidlearn_game/macos/Flutter/GeneratedPluginRegistrant.swift
+++ kidlearn_game/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -9,10 +9,12 @@ import flutter_tts
 import google_sign_in_ios
 import path_provider_foundation
 import shared_preferences_foundation
+import soundpool_macos
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FlutterTtsPlugin.register(with: registry.registrar(forPlugin: "FlutterTtsPlugin"))
   FLTGoogleSignInPlugin.register(with: registry.registrar(forPlugin: "FLTGoogleSignInPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
   SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
+  SwiftSoundpoolPlugin.register(with: registry.registrar(forPlugin: "SwiftSoundpoolPlugin"))
 }
