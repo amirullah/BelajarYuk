b8bbf0efd5eea0c95693a6725938a3fa149aff2f
Amirullah <amir@pnl.ac.id>
2026-07-12 00:56:09 +0000

    Perbarui registrant plugin (url_launcher) untuk platform desktop

--- kidlearn_game/macos/Flutter/GeneratedPluginRegistrant.swift
+++ kidlearn_game/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -10,6 +10,7 @@ import google_sign_in_ios
 import path_provider_foundation
 import shared_preferences_foundation
 import soundpool_macos
+import url_launcher_macos
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FlutterTtsPlugin.register(with: registry.registrar(forPlugin: "FlutterTtsPlugin"))
@@ -17,4 +18,5 @@ func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
   SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
   SwiftSoundpoolPlugin.register(with: registry.registrar(forPlugin: "SwiftSoundpoolPlugin"))
+  UrlLauncherPlugin.register(with: registry.registrar(forPlugin: "UrlLauncherPlugin"))
 }
