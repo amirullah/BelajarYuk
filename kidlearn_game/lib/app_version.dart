85aa2ad95965f3cb19cdf18bc3736aed102b5b5a
Amirullah <amir@pnl.ac.id>
2026-07-13 23:04:10 +0000

    v2.4.0+51: Uku umpan balik, lencana leaderboard, kunci ganti profil, avatar tier baru

--- kidlearn_game/lib/app_version.dart
+++ kidlearn_game/lib/app_version.dart
@@ -1,6 +1,6 @@
 /// Versi aplikasi yang TERPASANG. Dibump tiap rilis agar deteksi update akurat.
 /// [build] harus sama dengan angka setelah '+' di pubspec.yaml (versionCode).
 class AppVersion {
-  static const String name = '2.3.5';
-  static const int build = 50;
+  static const String name = '2.4.0';
+  static const int build = 51;
 }
