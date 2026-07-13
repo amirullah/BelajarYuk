13b038ee5119fe600329ccbb2b43aa7c8e61d59a
Amirullah <amir@pnl.ac.id>
2026-07-13 16:49:40 +0000

    v2.3.5+50: notif update setiap buka app, leaderboard all-time
    - Notif pembaruan kini muncul setiap kali aplikasi dibuka
    - Papan peringkat menampilkan skor terbaik sepanjang waktu (bukan mingguan)

--- kidlearn_game/lib/app_version.dart
+++ kidlearn_game/lib/app_version.dart
@@ -1,6 +1,6 @@
 /// Versi aplikasi yang TERPASANG. Dibump tiap rilis agar deteksi update akurat.
 /// [build] harus sama dengan angka setelah '+' di pubspec.yaml (versionCode).
 class AppVersion {
-  static const String name = '2.3.4';
-  static const int build = 49;
+  static const String name = '2.3.5';
+  static const int build = 50;
 }
